644c9a38f2833e428e9c36b82f8fbedd93c7d582
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 11:24:50 +0545

    add and area circle

--- app/src/main/java/com/e/fragments/MainActivity.java
+++ app/src/main/java/com/e/fragments/MainActivity.java
@@ -1,13 +1,53 @@
 package com.e.fragments;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import fragments.FirstFragment;
+import fragments.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    private Button btnFragment;
+    private Boolean status = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFragment = findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (status) {
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.add(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load second fragment");
+            status = false;
+      }
+        else
+        {
+            SecondFragment secondFragment = new SecondFragment();
+            fragmentTransaction.add(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load first Fragment");
+            status = true;
+        }
     }
 }
